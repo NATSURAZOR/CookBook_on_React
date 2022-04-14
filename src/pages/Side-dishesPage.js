ff7df0d815756c0a885003a70036bc2186e8fa68
NATSURAZOR <ulyan.super2405@gmail.com>
2022-04-14 14:34:48 +0200

    add simple css to side-dish and ingredients

--- src/pages/Side-dishesPage.js
+++ src/pages/Side-dishesPage.js
@@ -2,27 +2,41 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { api } from "../api";
 import { Spinner, Alert } from 'reactstrap';
+import "./Side-dishesPage.css";
 
 export function SidedishesPage(){
   const [sideDishes, setSideDishes] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState();
 
+
   useEffect(() => {
     setLoading(true);
 
-    api
-      .get('/recipes')
-      .then((res) => {
-        const sideD = res.data.map((recipe) => recipe.sideDish);
-        const filteredSideD = sideD.filter((element) => element !== undefined);
-
-        setSideDishes(filteredSideD);
-      })
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
-
+    api.get(`/recipes/side-dishes`)
+    .then((res) => {
+      setSideDishes(res.data);
+    })
+    .catch((error) => setError(error))
+    .finally(() => setLoading(false));
   }, [])
+
+
+  // useEffect(() => {
+  //   setLoading(true);
+
+  //   api
+  //     .get('/recipes')
+  //     .then((res) => {
+  //       const sideD = res.data.map((recipe) => recipe.sideDish);
+  //       const filteredSideD = sideD.filter((element) => element !== undefined);
+
+  //       setSideDishes(filteredSideD);
+  //     })
+  //     .catch((error) => setError(error))
+  //     .finally(() => setLoading(false));
+
+  // }, [])
 
 
   if (isLoading) {
@@ -36,12 +50,14 @@ export function SidedishesPage(){
   console.log(sideDishes);
 
   return (
-    <div>
+    <div className="side-dish-page">
+      <h2>List of Side Dish:</h2>
+      <ul>
       {sideDishes?.map((item) => (
-        <p>{item}</p>
+        <li>{item}</li>
         )
       )}
-
+      </ul>
     </div>
   );
 
