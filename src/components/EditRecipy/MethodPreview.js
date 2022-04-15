03de3c84bbc12ee5a96613c230892088fbda3bf7
NATSURAZOR <ulyan.super2405@gmail.com>
2022-04-15 15:47:01 +0200

    MethodPreview

--- src/components/EditRecipy/MethodPreview.js
+++ src/components/EditRecipy/MethodPreview.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 export function MethodPreview({newRecipe}){
 
   return (
   <div className="MethodPreview-section">
     <h1>Method Preview</h1>
-    <p>{newRecipe.directions}</p>
+    <ReactMarkdown className='reactmarckDown'>{newRecipe.directions}</ReactMarkdown>
   </div>
   );
 }
